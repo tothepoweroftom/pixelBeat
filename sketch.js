75e929bce2df09d9ad98db8dd800d1e4dc9710cc
tothepoweroftom <powerth@tcd.ie>
2016-07-07 20:10:31 +0100

    Added Beats

--- sketch.js
+++ sketch.js
@@ -14,8 +14,8 @@ var counter;
 
 //AUDIO
 var synth = new Tone.FMSynth({
-  "harmonicity": 2.21,
-  "modulationIndex": 20,
+  "harmonicity": 3.01,
+  "modulationIndex": 10,
 
   "carrier": {
     "volume": 0,
@@ -27,12 +27,12 @@ var synth = new Tone.FMSynth({
     "envelope": {
       "attack": 0.1,
       "decay": 1,
-      "sustain": 0.2,
+      "sustain": 0.25,
       "release": 0.5,
     },
 
     "filterEnvelope": {
-      "attack": 0.01,
+      "attack": 0.1,
       "decay": 0.0,
       "sustain": 1,
       "release": 0.5,
@@ -76,10 +76,45 @@ var synth = new Tone.FMSynth({
 //     "release": 0.5,
 //   }
 // }).toMaster();
+var sampler
+
+function preload() {
+  sampler = new Tone.Sampler({
+    A: {
+      1: "./audio/anomaly_1.wav",
+      2: "./audio/anomaly_2.wav",
+      3: "./audio/anomaly_3.wav",
+      4: "./audio/anomaly_4.wav",
+      5: "./audio/anomaly_5.wav",
+      6: "./audio/anomaly_6.wav",
+      7: "./audio/anomaly_7.wav",
+      8: "./audio/anomaly_8.wav",
+      9: "./audio/anomaly_9.wav",
+      10: "./audio/anomaly_10.wav",
+      11: "./audio/anomaly_12.wav",
+      12: "./audio/anomaly_12.wav",
+      13: "./audio/anomaly_13.wav",
+      14: "./audio/anomaly_14.wav",
+      15: "./audio/anomaly_15.wav",
+      16: "./audio/anomaly_16.wav",
+      17: "./audio/anomaly_17.wav",
+      18: "./audio/anomaly_18.wav",
+      19: "./audio/anomaly_19.wav",
+      20: "./audio/anomaly_20.wav",
+      21: "./audio/anomaly_21.wav",
+      22: "./audio/anomaly_22.wav",
+      23: "./audio/anomaly_23.wav",
+      24: "./audio/anomaly_24.wav",
+
+
+    }
+  }).toMaster();
+
+}
 
 function setup() {
   canvas = createCanvas(640, 360);
-  stepSize = (width) / 16;
+  stepSize = (width) / 64;
   counter = 0;
 
   // specify multiple formats for different browsers
@@ -87,14 +122,14 @@ function setup() {
     'assets/fingers.webm'
   ]);
   fingers.loop();
-  fingers.hide();
+  //fingers.hide();
   noStroke();
   fill(0);
-  frameRate(24);
+  frameRate(14);
 }
 
 function draw() {
-  background(255);
+  //background(255);
   fingers.loadPixels();
 
 
@@ -102,53 +137,77 @@ function draw() {
     for (var x = 0; x < width; x += stepSize) {
       var id = y * width + x;
       var darkness = (255 - fingers.pixels[id * 4]) / 255;
-      var radius = width/16;
+      var radius = width / 16;
       if (darkness == 1) {
-        print("x= " + x + " y= " + y);
+        //print("x= " + x + " y= " + y);
         switch (x) {
           case 0:
-            break;
-          case 40:
-            break;
-          case 80:
-            break;
-          case 120:
-            synth.triggerAttackRelease("A3", "8n");
-            break;
-          case 160:
-            synth.triggerAttackRelease("C3", "8n");
+            sample.triggerAttackRelease("A.1", "4n");
 
             break;
-          case 200:
-            synth.triggerAttackRelease("D3", "8n");
+          case radius * 1:
+            synth.triggerAttackRelease("C5", "4n");
+            sampler.triggerAttackRelease("A.2", "4n");
 
             break;
-          case 240:
-            synth.triggerAttackRelease("E3", "8n");
+          case radius * 2:
+            synth.triggerAttackRelease("E5", "4n");
+            sampler.triggerAttackRelease("A.3", "4n");
 
             break;
-          case 280:
-            synth.triggerAttackRelease("G3", "8n");
+          case radius * 3:
+            synth.triggerAttackRelease("A3", "4n");
+                        sampler.triggerAttackRelease("A.4", "4n");
 
             break;
-          case 320:
-            synth.triggerAttackRelease("A4", "8n");
+          case radius * 4:
+            synth.triggerAttackRelease("B3", "4n");
+            sampler.triggerAttackRelease("A.4", "4n");
+
 
             break;
-          case 360:
-            synth.triggerAttackRelease("C4", "8n");
+          case radius * 5:
+            synth.triggerAttackRelease("C3", "4n");
+            sampler.triggerAttackRelease("A.5", "4n");
+
 
             break;
-          case 400:
-            synth.triggerAttackRelease("D4", "8n");
+          case radius * 6:
+            synth.triggerAttackRelease("D3", "4n");
+            sampler.triggerAttackRelease("A.6", "4n");
+
 
             break;
-          case 440:
-            synth.triggerAttackRelease("E4", "8n");
+          case radius * 7:
+            synth.triggerAttackRelease("G3", "4n");
+                        sampler.triggerAttackRelease("A.7", "4n");
+
 
             break;
-          case 480:
-            synth.triggerAttackRelease("G4", "8n");
+          case radius * 8:
+            synth.triggerAttackRelease("A4", "4n");
+            sampler.triggerAttackRelease("A.8", "4n");
+
+
+            break;
+          case radius * 9:
+            synth.triggerAttackRelease("C4", "4n");
+            sampler.triggerAttackRelease("A.9", "4n");
+
+            break;
+          case radius * 10:
+            synth.triggerAttackRelease("D4", "4n");
+            sampler.triggerAttackRelease("A.10", "4n");
+
+            break;
+          case radius * 11:
+            synth.triggerAttackRelease("E4", "4n");
+            sampler.triggerAttackRelease("A.11", "4n");
+
+            break;
+          case radius * 12:
+            synth.triggerAttackRelease("G4", "4n");
+            sampler.triggerAttackRelease("A.12", "4n");
 
             break;
 
