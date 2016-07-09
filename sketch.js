75d9567cbf07ee760f4afe576167876920151863
tothepoweroftom <powerth@tcd.ie>
2016-07-09 15:42:16 +0100

    Music Creation System initiated!

--- sketch.js
+++ sketch.js
@@ -17,8 +17,12 @@ var slider
 var vscale = 64;
 var rects = [];
 
-var noX = canvas.width / vscale;
-var noY = canvas.height / vscale;
+
+var notes = ["A.1", "A.2", "A.3", "A.4", "A.5", "A.6",
+  "B.1", "B.2", "B.3", "B.4", "B.5", "B.6",
+  "C.1", "C.2", "C.3", "C.4", "C.5", "C.6",
+  "D.1", "D.2", "D.3", "D.4", "D.5", "D.6"
+];
 
 function Rectangle(_x, _y, _width, _height, _sample) {
   this.x = _x;
@@ -55,12 +59,34 @@ function preload() {
 
     },
     B: {
-      1: "./audio/piano/piano1.wav",
-      2: "./audio/piano/piano2.wav",
-      3: "./audio/piano/piano3.wav",
-      4: "./audio/piano/piano4.wav",
-      5: "./audio/piano/piano5.wav",
-      6: "./audio/piano/piano6.wav",
+      1: "./audio/cello/cello1.wav",
+      2: "./audio/cello/cello2.wav",
+      3: "./audio/cello/cello3.wav",
+      4: "./audio/cello/cello4.wav",
+      5: "./audio/cello/cello5.wav",
+      6: "./audio/cello/cello6.wav",
+
+
+
+    },
+    C: {
+      1: "./audio/hang/hang1.wav",
+      2: "./audio/hang/hang2.wav",
+      3: "./audio/hang/hang3.wav",
+      4: "./audio/hang/hang4.wav",
+      5: "./audio/hang/hang5.wav",
+      6: "./audio/hang/hang6.wav",
+
+
+
+    },
+    D: {
+      1: "./audio/violin/violin1.wav",
+      2: "./audio/violin/violin2.wav",
+      3: "./audio/violin/violin3.wav",
+      4: "./audio/violin/violin4.wav",
+      5: "./audio/violin/violin5.wav",
+      6: "./audio/violin/violin6.wav",
 
 
 
@@ -89,8 +115,9 @@ function setup() {
   for (var y = 0; y < video.height; y++) {
     for (var x = 0; x < video.width; x++) {
       var index = (x + (y * video.width));
-      rects[index] = new Rectangle(x * vscale, y * vscale, vscale, vscale, x);
+      rects[index] = new Rectangle(x * vscale, y * vscale, vscale, vscale, notes[index]);
       rects[index].display();
+      print(notes[index]);
     }
   }
 }
@@ -111,9 +138,7 @@ function draw() {
       if (bright > 220) {
 
         rects[index / 4].displayTrigger();
-        var sampleNo = String(rects[index / 4].sample % 6 + 1);
-        var note = "A." + sampleNo;
-        sampler.triggerAttack(note);
+        sampler.triggerAttack(rects[index/4].sample);
         print("x,y = " + x + " " + y);
       }
 
